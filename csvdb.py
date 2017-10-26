29a74141ee4d2cee8debbe6641e0f27f8298a129
ww <winslow.williams>
2017-10-26 00:02:12 -0400

    Added SelectExpression

--- csvdb.py
+++ csvdb.py
@@ -15,6 +15,18 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+
+SELECT_STR_U = 'SELECT'
+SELECT_STR_L = 'select'
+WHERE_STR_U = 'WHERE'
+WHERE_STR_L = 'where'
+EQ = '='
+LT = '<'
+LTEQ = '<='
+GT = '>'
+GTEQ = '>='
+ALL = '*'
+
 class TableException(Exception):
   pass
 
@@ -26,6 +38,133 @@ class InvalidRow(TableException):
 
 class InvalidRowIndex(TableException):
   pass
+
+class InvalidSelectExpression(TableException):
+  pass
+
+class SelectExpression(object):
+
+  def __init__(self,text,table):
+    self.text = text
+    self.table = table
+    self.select_mnemonic = False
+    self.column_expression = None
+    self.columns = list()
+    self.where_mnemonic = False
+    self.where_name = None
+    self.operator = None
+    self.where_value = None
+
+    self.select_offset = -1
+    self.where_offset = -1
+    self.column_expression_offset = -1
+    self.where_name_offset = -1
+    self.operator_offset = -1
+    self.where_value_offset = -1
+
+    if SELECT_STR_U in text:
+      self.select_mnemonic = True
+      self.select_offset = text.find(SELECT_STR_U)
+      self.column_expression_offset = self.select_offset + len(SELECT_STR_U)
+    if SELECT_STR_L in text:
+      self.select_mnemonic = True
+      self.select_offset = text.find(SELECT_STR_L)
+      self.column_expression_offset = self.select_offset + len(SELECT_STR_L)
+    if WHERE_STR_U in text:
+      self.where_mnemonic = True
+      self.where_offset = text.find(WHERE_STR_U)
+      self.where_name_offset = self.where_offset + len(WHERE_STR_U)
+    if WHERE_STR_L in text:
+      self.where_mnemonic = True
+      self.where_offset = text.find(WHERE_STR_L)
+      self.where_name_offset = self.where_offset + len(WHERE_STR_L)
+
+    if EQ in text:
+      self.operator = EQ
+      self.operator_offset = text.find(EQ)
+      self.where_value_offset = self.operator_offset + len(EQ)
+      self.where_value = text[self.where_value_offset:].strip()
+    elif LT in text:
+      self.operator = LT
+      self.operator_offset = text.find(LT)
+      self.where_value_offset = self.operator_offset + len(LT)
+      self.where_value = text[self.where_value_offset:].strip()
+    elif LTEQ in text:
+      self.operator = LTEQ
+      self.operator_offset = text.find(LTEQ)
+      self.where_value_offset = self.operator_offset + len(LTEQ)
+      self.where_value = text[self.where_value_offset:].strip()
+    elif GT in text:
+      self.operator = GT
+      self.operator_offset = text.find(GT)
+      self.where_value_offset = self.operator_offset + len(GT)
+      self.where_value = text[self.where_value_offset:].strip()
+    elif GTEQ in text:
+      self.operator = GTEQ
+      self.operator_offset = text.find(GTEQ)
+      self.where_value_offset = self.operator_offset + len(GTEQ)
+      self.where_value = text[self.where_value_offset:].strip()
+
+    if self.where_mnemonic:
+      self.column_expression = text[self.column_expression_offset:self.where_offset].strip()
+      if self.operator_offset > self.where_name_offset:
+        self.where_name = text[self.where_name_offset:self.operator_offset].strip()
+      if self.where_value_offset > self.operator_offset:
+        self.where_value = text[self.where_value_offset:].strip()
+    else:
+      self.column_expression = text[self.column_expression_offset:].strip()
+
+    if ALL == self.column_expression:
+      for col in table.getHeader():
+        self.columns.append(col)
+    else:
+      parts = self.column_expression.split(',')
+      for part in parts:
+        self.columns.append(part.strip())
+
+  def isValid(self):
+    if False == self.select_mnemonic:
+      return False
+    if ALL != self.column_expression:
+      header = self.table.getHeader()
+      for col in self.columns:
+        if False == (col in header):
+          return False
+    if self.where_mnemonic:
+      if None is self.operator:
+        return False
+      if None is self.where_value:
+        return False
+    return True
+
+  def getSelectColumns(self):
+    return self.columns
+
+  def getWhereColumn(self):
+    return self.where_name
+
+  def getText(self):
+    return self.text
+
+  def checkMatch(self,value):
+    rv = False
+    if EQ == self.operator:
+      if value == self.where_value:
+        rv = True
+    elif LT == self.operator:
+      if value < self.where_value:
+        rv = True
+    elif LTEQ == self.operator:
+      if value <= self.where_value:
+        rv = True
+    elif GT == self.operator:
+      if value > self.where_value:
+        rv = True
+    elif GTEQ == self.operator:
+      if value >= self.where_value:
+        rv = True
+    return rv 
+
 
 class CSVTable:
 
@@ -137,3 +276,28 @@ class CSVTable:
       if value not in rv:
         rv.append(value)
     return rv  
+
+  def selectWithSelectExpression(self,se):
+    rv = list()
+    if se.isValid():
+      where_index = self.getHeaderIndex(se.getWhereColumn())
+      column_indices = list()
+      table = self.reset()
+      for col in se.getSelectColumns():
+        column_indices.append(self.getHeaderIndex(col))
+      for row in table.getIter():
+        append = False
+        if where_index > -1:
+          append = se.checkMatch(row[where_index])
+        else:
+          append = True
+        if append:
+          v = list()
+          for i in column_indices:
+            v.append(row[i])
+          rv.append(v)
+    else:
+      raise InvalidSelectExpression(self.text)
+    return rv
+
+
